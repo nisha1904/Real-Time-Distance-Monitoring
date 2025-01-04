f73c17ad4d9d1dd32e9535497db3a03d6f1a8c7b
nisha1904 <nisha1922004@gmail.com>
2025-01-05 02:18:54 +0530

    Add files via upload

---
+++ Raspberry_pi_pico.ino
@@ -0,0 +1,68 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "****";
+const char* password = "****";
+const char* mqtt_server = "****";
+const char* mqtt_user = "****"
+const char* mqtt_password = "****";
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("PicoClient", mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      client.subscribe("ultrasonic/distance");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
